fb95e7ede8520abb4d23e93d95edbca6434554b9
louey <ng.looyee@gmail.com>
2020-12-29 19:23:03 +1100

    before deployment1

--- babydojo/settings.py
+++ babydojo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yhayt81rwsaf+&nt5w9j(oo+u&(ov-!h-=q-mc8j64h_mg&^%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['babydojo-casual.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -129,6 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
