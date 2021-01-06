8a10af91c88e7ad8c0e99f23f1b23d7780bc5d6a
louey <ng.looyee@gmail.com>
2021-01-06 17:50:08 +1100

    Try

--- babydojo/settings.py
+++ babydojo/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'babydojo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'babydojo_casual',
-#        'USER':'babydojo_admin',
-#        'PASSWORD':'369852Ng',
-#        'HOST':'database-3.c41coxfhgq67.us-east-2.rds.amazonaws.com',
-#        'PORT':'5432'
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'babydojo_casual',
+        'USER':'babydojo_admin',
+        'PASSWORD':'369852Ng',
+        'HOST':'database-3.c41coxfhgq67.us-east-2.rds.amazonaws.com',
+        'PORT':'5432'
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 #import dj_database_url
 #db_from_env = dj_database_url.config(conn_max_age=600)
