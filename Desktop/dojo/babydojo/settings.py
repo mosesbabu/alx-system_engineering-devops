5cf6f28c71d9bfefc9808ca41e8be4075b6e4eb2
moselamar <44118179+moselamar@users.noreply.github.com>
2021-01-06 17:53:19 +0300

    changed database configs

--- Desktop/dojo/babydojo/settings.py
+++ Desktop/dojo/babydojo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,7 +106,7 @@ DATABASES = {
 }
 '''
 
-import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
