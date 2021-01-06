b023582c26df262701993b660db1cb70723b6bec
louey <ng.looyee@gmail.com>
2021-01-06 12:15:49 +1100

    PostgreSQL Setting

--- babydojo/settings.py
+++ babydojo/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'babydojo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'babydojo_casual',
-#        'USER':'babydojo_admin',
-#        'PASSWORD':'369852Ng',
-#        'HOST':'database-3.c41coxfhgq67.us-east-2.rds.amazonaws.com',
-#        'PORT':'5432'
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'babydojo_casual',
+        'USER':'babydojo_admin',
+        'PASSWORD':'369852Ng',
+        'HOST':'database-3.c41coxfhgq67.us-east-2.rds.amazonaws.com',
+        'PORT':'5432'
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
